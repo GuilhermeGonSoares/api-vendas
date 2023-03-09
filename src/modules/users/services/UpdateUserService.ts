dc519ba4779193e481a4a7080e46bacc8786e765
Guilherme Gonçalves <guilherme16.gon@gmail.com>
2023-03-09 09:10:31 -0300

    service para atualizar o profile do user

---
+++ src/modules/users/services/UpdateUserService.ts
@@ -0,0 +1,52 @@
+import AppError from '@shared/errors/AppError';
+import { compare, hash } from 'bcryptjs';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  id: string;
+  email: string;
+  name: string;
+  password?: string;
+  old_password?: string;
+}
+
+export default class UpdateUserService {
+  public async execute({ id, email, password, name, old_password }: IRequest) {
+    const userRepository = getCustomRepository(UsersRepository);
+
+    const user = await userRepository.findOne(id);
+
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
+    if (user.email !== email) {
+      const emailExists = await userRepository.findByEmail(email);
+      if (emailExists) {
+        throw new AppError('Email already exists.');
+      }
+    }
+
+    if (password && !old_password) {
+      throw new AppError('Old password is required.');
+    }
+
+    if (password && old_password) {
+      const passwordConfirmation = await compare(old_password, user.password);
+
+      if (!passwordConfirmation) {
+        throw new AppError('Password invalid.');
+      }
+
+      user.password = await hash(password, 8);
+    }
+
+    user.name = name;
+    user.email = email;
+
+    userRepository.save(user);
+
+    return user;
+  }
+}
